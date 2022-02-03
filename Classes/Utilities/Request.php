e619450e4f16160b04ec2c90ac22e84c18425585
David Bascom <david@99grad.de>
2022-02-03 16:24:12 +0100

    Bugfix and cleanup getBasicAuth()

--- Classes/Utilities/Request.php
+++ Classes/Utilities/Request.php
@@ -239,19 +239,26 @@ class Request implements SingletonInterface {
 	 * @return array
 	 */
 	public function getBasicAuth() {
+
+		$username = '';
+		$password = '';
+
 		if (isset($_SERVER['PHP_AUTH_USER'])) {
             $username = $_SERVER['PHP_AUTH_USER'];
             $password = $_SERVER['PHP_AUTH_PW'];
 		} else {
 			$check = ['HTTP_AUTHENTICATION', 'HTTP_AUTHORIZATION', 'REDIRECT_HTTP_AUTHORIZATION'];
-			[$username, $password] = current(array_filter( $check, function ( $key ) {
-				if ($value = $_SERVER[$key] ?? false) {
-					if (strpos(strtolower($value), 'basic') === 0) {
-						return explode(':', base64_decode(substr($value, 6)));
-					}
+			foreach ($check as $key) {
+				$value = $_SERVER[$key] ?? false;
+				$isBasic = strpos(strtolower($value), 'basic') === 0;
+				if ($value && $isBasic) {
+					$decodedValue = base64_decode(substr($value, 6));
+					[$username, $password] = explode(':', $decodedValue) ?: ['', ''];
+					break;
 				}
-			})) ?: ['', ''];
+			}
 		}
+
 		if (!$username && !$password) return [];
 		return ['username'=>$username, 'password'=>$password];
 	}
