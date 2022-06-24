dc930b2674243402adbe73f71114ab8a017a5dd2
David Bascom <david@99grad.de>
2022-06-24 21:32:18 +0200

    Update version

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,17 +1,17 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Typo3 Helpers by 99°',
-    'description' => "Why write two lines of code, if one is enough? A huge collection of very smart 'oneliners' to significantly speed up your extension development and make updating to new Typo3 versions simple. Packed with things you've spent hours searching on the internet for. Everything accessible with one click via a convenient backend module. No mess and no conflicts. Use whatever method makes you happy – or simple grab the source-code and paste it in your own extension.",
+    'title' => 'TYPO3 Helpers by 99°',
+    'description' => "Drastically speed up your extension development with this huge collection of very smart 'oneliners'. Make updating to new TYPO3 versions simple. Packed with things you've spent hours searching on the internet for. Everything accessible with one click via a convenient backend module. No mess and no conflicts. Use whatever method makes you happy – or simple grab the source-code and paste it in your own extension.",
     'category' => 'services',
-    'author' => 'www.99grad.de, David Bascom, Marc Maaß, Martin Wagner',
+    'author' => 'www.99grad.de, David Bascom, Marc Maaß',
     'author_email' => 'david@99grad.de',
     'state' => 'stable',
     'internal' => '',
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.7.2',
+    'version' => '1.7.3',
     'constraints' => [
         'depends' => [
             'typo3' => '8.6.0-11.9.99',
