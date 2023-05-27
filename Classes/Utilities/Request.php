b3c40ee562c516293ac32bc7d914f5efe7dcc08f
David Bascom <david@99grad.de>
2023-05-27 13:21:41 +0200

    [BUGFIX] fixed warning

--- Classes/Utilities/Request.php
+++ Classes/Utilities/Request.php
@@ -286,6 +286,60 @@ class Request implements SingletonInterface {
 			'content' 	=> $result,
 		];
 	}
+	
+	/**
+	 * Sendet einen DELETE Request (per curl) an einen Server
+	 * ```
+	 * \nn\t3::Request()->DELETE( 'https://...', ['a'=>'123'] );
+	 * \nn\t3::Request()->DELETE( 'https://...', ['a'=>'123'], ['Accept-Encoding'=>'gzip, deflate'] );
+	 * ```
+	 * @param string $url
+	 * @param array $queryParams
+	 * @param array $headers
+	 * @return array
+	 */
+	public function DELETE( $url = '', $queryParams = [], $headers = [] ) {
+
+		// ['Accept-Encoding'=>'gzip'] --> ['Accept-Encoding: gzip']
+		array_walk( $headers, function (&$v, $k) {
+			if (!is_numeric($k)) $v = $k . ': ' . $v;
+		});
+
+		$headers = array_values($headers);
+		$url = $this->mergeGetParams($url, $queryParams);
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL, $url );
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers );
+
+		// follow redirects
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+		$result = curl_exec($ch);
+		$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		$error = curl_error($ch);
+
+		curl_close($ch);
+
+		if ($httpcode >= 300) {
+			return [
+				'error'		=> true,
+				'status' 	=> $httpcode,
+				'content'	=> $error,
+				'response'	=> @json_decode($result, true) ?: $result,
+			];
+		}
+		
+		return [
+			'error'		=> false,
+			'status' 	=> 200, 
+			'content' 	=> $result,
+		];
+	}
 
 	/**
 	 * 
@@ -293,7 +347,7 @@ class Request implements SingletonInterface {
 	public function mergeGetParams( $url = '', $getParams = [] ) {
 		$parts = parse_url($url);
 		$getP = [];
-		if ($parts['query']) {
+		if ($parts['query'] ?? false) {
 			parse_str($parts['query'], $getP);
 		}
 		ArrayUtility::mergeRecursiveWithOverrule($getP, $getParams, true, true );
